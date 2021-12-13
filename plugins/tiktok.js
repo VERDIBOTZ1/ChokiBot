49cdc2cfd70876c33035b81e77df7d6f9535a7e7
RTeam1 <77674437+RTeam1@users.noreply.github.com>
2021-12-13 19:50:58 +0700

    Add files via upload

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,18 +1,29 @@
 let fetch = require('node-fetch')
-
-let handler = async (m, { itsu, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
-  m.reply('Loading...')
-  let res = await fetch(API('Velgrynd', '/api/tiktok2', { url: args[0] }))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  let url = json.video_original
-  itsu.sendFile(m.chat, url, 'tiktok.mp4', '', m)
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'Uhm..url nya mana?'
+m.reply(wait)
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+let { description, author, statistic, link } = json.result
+await conn.sendFile(m.chat, link, 'tt.mp4', `
+▶ ${statistic.playCount} Views
+❤ ${statistic.diggCount} Likes
+🔁 ${statistic.shareCount} Shares
+💬 ${statistic.commentCount} Comments
+- *By:* ${author.nickname} (${author.username})
+- *Desc:*
+${description}
+`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+
+handler.help = ['tiktok <url>']
 handler.tags = ['downloader']
+//handler.command = /^tiktok$/i
 handler.command = /^(tik(tok)?(dl)?)$/i
-
 handler.limit = true
-
 module.exports = handler
+
+
+//await conn.sendFile(m.chat, url, 'tiktok.mp4', kasihcaption, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
---
+++ plugins/tiktok2.js
@@ -0,0 +1,18 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { itsu, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  m.reply('Loading...')
+  let res = await fetch(API('Velgrynd', '/api/tiktok2', { url: args[0] }))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  let url = json.video_original
+  itsu.sendFile(m.chat, url, 'tiktok.mp4', '', m)
+}
+handler.help = ['tiktok2'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tik(tok2)?(dl)?)$/i
+
+handler.limit = true
+
+module.exports = handler
