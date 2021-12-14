fafdf09bf9db2e9ebab7328b60c4b9ff16e2e037
RTeam1 <77674437+RTeam1@users.noreply.github.com>
2021-12-14 14:35:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ function kyun(seconds){
     return (s < 10 ? '0' : '') + s;
   }
   var hours = Math.floor(seconds / (60*60));
-  var minutes = Math.floor(seconds % (60*60) / 60);
+  var minutes = Math.floor(seconds % (60*60) / 60); 
   var seconds = Math.floor(seconds % 60);
 
   //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
